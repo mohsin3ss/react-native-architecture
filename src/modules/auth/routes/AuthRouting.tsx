0d424044d152b20907e4e897632c1ea54beab0ed
Mohsin Khan <mohsink9.3ss@gmail.com>
2023-01-24 20:26:50 +0500

    feat: Initial Commit

---
+++ src/modules/auth/routes/AuthRouting.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AuthRouting = () => {
+  return (
+    <View>
+      <Text>AuthRouting</Text>
+    </View>
+  );
+};
+
+export default AuthRouting;
